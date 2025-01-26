f30a330f0ac3a19f4fdb9b72dca7b05cdac8ce52
DFtorres <dftorres2004@gmail.com>
2025-01-26 00:06:39 -0500

    Deleted unnecessary app controller and service, and added missing Modules (Notes, Users, ORM)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersHttpModule } from './users/usershttp.module';
+import { ConfigModule } from '@nestjs/config';
+import { NotesHttpModule } from './notes/noteshttp.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    NotesHttpModule,
+    UsersHttpModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT || 5432),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + './**/*.entity{.ts,.js}'],
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+  ],
 })
 export class AppModule {}
